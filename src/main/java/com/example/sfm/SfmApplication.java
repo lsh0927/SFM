8159772493fa8b6b700882a40b738ed28c33e8a1
lsheon0927@naver.com <lsheon0927@naver.com>
2024-03-06 16:00:03 +0900

    Initial commit

---
+++ src/main/java/com/example/sfm/SfmApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfmApplication.class, args);
+    }
+
+}
