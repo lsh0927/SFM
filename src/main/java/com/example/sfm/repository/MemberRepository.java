bbd776752edcddd0615e9d41029c2228a247925e
lsheon0927@naver.com <lsheon0927@naver.com>
2024-03-16 19:02:20 +0900

    FIX: 구현할 기능 명시

--- src/main/java/com/example/sfm/repository/MemberRepository.java
+++ src/main/java/com/example/sfm/repository/MemberRepository.java
@@ -4,10 +4,17 @@ import com.example.sfm.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
-    boolean existsByName(String name);
+    boolean existsByName(String name); // 이름이 중복되는지 확인하는 메소드
 
-    //JPA의 JpaRepository 매개변수, 인터페이스 명이 일치한 지 확인하면 된다.
+    Optional<Member> findByName(String name); // 이름으로 회원을 찾는 메소드
+    //impl을 구현해서 Override해야함
+
+    //이름으로 찾기
+    //id로 찾기
+    //list 반환 메서드 필요?
     //
 }
